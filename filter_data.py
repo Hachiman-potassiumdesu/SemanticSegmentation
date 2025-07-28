676cb7fbd92614ea5e4cfe2da820ca5d842f2aa2
Kevin Geng <79676699+KevinGeng07@users.noreply.github.com>
2025-07-28 15:19:35 -0700

    Update filter_data.py

--- filter_data.py
+++ filter_data.py
@@ -5,10 +5,7 @@ import os
 import pandas as pd
 
 ### In total, four datasets are created. Two with the Train/Val/Test splits combining data from Austria, Belgium, & Luxembourg,
-### and two independent test sets combinding data from Kosovo and Switzerland.
-input_dataset = 'BigEarthNet-S2'
-reference_dataset = 'Reference_Maps'
-
+### and two independent test sets combinding data from Kosovo and Switzerland. EACH DATASET NEEDS TO BE CREATED SEPARATELY.
 output_dataset = 'out_dataset'
 moved_dataset = 'natural_ecosystem'
 
@@ -20,10 +17,10 @@ for country in ['Austria', 'Belgium', 'Luxembourg']:
     print(country)
 
     ### These classes are used to check for overlap, with the class labels.
-    # Natural Ecosystem classes: [311, 312, 313, 321, 322, 324, 333, 411, 412, 511, 512]
-    # Artificial Structures classes: [111, 112, 121, 122, 131, 133, 141, 142, 211, 221, 222, 231, 242, 243]
+    ### Natural Ecosystem classes: [311, 312, 313, 321, 322, 324, 333, 411, 412, 511, 512]
+    ### Artificial Structures classes: [111, 112, 121, 122, 131, 133, 141, 142, 211, 221, 222, 231, 242, 243]
     unique_classes = [311, 312, 313, 321, 322, 324, 333, 411, 412, 511, 512]
-    class_pixels = {k: 0 for k in unique_classes}
+    # class_pixels = {k: 0 for k in unique_classes}
 
     total_imgs = 0
     kept_imgs = 0
@@ -38,8 +35,8 @@ for country in ['Austria', 'Belgium', 'Luxembourg']:
         except: continue
 
         ### When counting the number of pixels in each country belonging to each class, use this code. Otherwise, exclude it.
-        # Natural Ecosystem excluded classes: [111, 112, 121, 122, 123, 124, 131, 132, 133, 141, 142, 211, 212, 213, 221, 222, 223, 231, 241, 242, 243, 244, 323, 331, 332, 334, 421, 422, 423, 521, 522, 523, 999]
-        # Artificial Structures excluded classes: [123, 124, 132, 212, 213, 223, 241, 244, 311, 312, 313, 321, 322, 323, 324, 331, 332, 333, 334, 335, 411, 412, 421, 422, 423, 511, 512, 521, 522, 523, 999]
+        ### Natural Ecosystem excluded classes: [111, 112, 121, 122, 123, 124, 131, 132, 133, 141, 142, 211, 212, 213, 221, 222, 223, 231, 241, 242, 243, 244, 323, 331, 332, 334, 421, 422, 423, 521, 522, 523, 999]
+        ### Artificial Structures excluded classes: [123, 124, 132, 212, 213, 223, 241, 244, 311, 312, 313, 321, 322, 323, 324, 331, 332, 333, 334, 335, 411, 412, 421, 422, 423, 511, 512, 521, 522, 523, 999]
         # bands[np.isin(bands, [111, 112, 121, 122, 123, 124, 131, 132, 133, 141, 142, 211, 212, 213, 221, 222, 223, 231, 241, 242, 243, 244, 323, 331, 332, 334, 421, 422, 423, 521, 522, 523, 999])] = 0
 
 
@@ -48,7 +45,7 @@ for country in ['Austria', 'Belgium', 'Luxembourg']:
             np.save(os.path.join(moved_dataset, row['split'], f'{row["patch_id"]}.npy'), bands)
             kept_imgs += 1
 
-        ### Count number of pixels per class.
+        ### Count number of pixels per class. To perform this, insert 0 to beginning of "unique_classes" and uncomment "class_pixels".
         # u_vals, counts = np.unique(bands, return_counts=True)
         # for u_val, count in zip(u_vals, counts):
         #     class_pixels[u_val] += int(count)
@@ -61,4 +58,4 @@ for country in ['Austria', 'Belgium', 'Luxembourg']:
     # for c, p in list(class_pixels.items())[1:]:
     #     print(c, p / (total_imgs * 120 * 120 - class_pixels[0]))
 
-    # print(f'Original Images: {total_imgs}\nKept Images: {kept_imgs}')
+    print(f'Original Images: {total_imgs}\nKept Images: {kept_imgs}')
